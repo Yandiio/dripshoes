38192c3abb1d2a0b7edd9c918bd43e09fc249d98
Fenanders <f.yandi1128@gmail.com>
2024-04-26 23:08:55 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uts_yandi_dripshoes/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/screens/components/myAppBar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:uts_yandi_dripshoes/constants.dart';
+import 'package:uts_yandi_dripshoes/screens/components/myIcon.dart';
+
+class MyAppBar extends StatelessWidget {
+  final IconData icon;
+  final bool leading;
+  final String title;
+  final Function onMenuTap;
+  MyAppBar({this.icon, this.leading = false, this.title, this.onMenuTap});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          IconButton(
+            icon: leading == false
+                ? Icon(
+                    icon,
+                    size: 8,
+                  )
+                : Icon(
+                    icon,
+                    size: 34,
+                  ),
+            onPressed: leading == false
+                ? onMenuTap
+                : () {
+                    Navigator.pop(context);
+                  },
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Text(
+              title,
+              style: TextStyle(
+                  fontSize: leading == true ? 22 : 18,
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          IconButton(
+            icon: Stack(
+              children: [
+                Icon(
+                  MyIcon.bag_1,
+                ),
+                Positioned(
+                  top: 0,
+                  right: -0.3,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 7,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 1),
+                      child: Text(
+                        "3",
+                        style: TextStyle(
+                            color: primaryColor,
+                            fontSize: 9,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
